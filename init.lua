27006c537fb51b0ca60e0255da97e9f71f855759
David Cohen <dcohen@hashicorp.com>
2023-03-21 12:15:46 -0400

    install some themes

--- init.lua
+++ init.lua
@@ -122,11 +122,34 @@ require('lazy').setup({
       },
     },
   },
-
+  {
+    'folke/tokyonight.nvim',
+    priority = 1,
+    config = function()
+      vim.cmd.colorscheme 'tokyonight'
+    end,
+  },
+  {
+    'ishan9299/nvim-solarized-lua',
+  },
+  {
+    'shaunsingh/nord.nvim',
+    priority = 9,
+    config = function()
+      vim.cmd.colorscheme 'nord'
+    end,
+  },
+  {
+    'dracula/vim',
+    priority = 10,
+    config = function()
+      vim.cmd.colorscheme 'dracula'
+    end,
+  },
   {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
-    priority = 1000,
+    priority = 11,
     config = function()
       vim.cmd.colorscheme 'onedark'
     end,
