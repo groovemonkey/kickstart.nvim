89ecdfe75a6d906a2a833117c7feb13ba942d8ba
David Cohen <dcohen@hashicorp.com>
2023-03-21 15:47:06 -0400

    switch to a bigger go plugin, with fillstructs and some other nice tools I miss

--- init.lua
+++ init.lua
@@ -92,9 +92,27 @@ require('lazy').setup({
   },
 
   -- dave: this looked like a more minimal go plugin
+  -- {
+  --    'crispgm/nvim-go',
+  --    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  --  },
   {
-    'crispgm/nvim-go',
-    dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+    -- big boy Go plugin -- I was missing fillstruct and a few other things
+    "ray-x/go.nvim",
+    dependencies = { -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = { "CmdlineEnter" },
+    ft = { "go", 'gomod' },
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    'buoto/gotests-vim',
   },
   {
     'ggandor/leap.nvim',
@@ -531,6 +549,18 @@ cmp.setup {
 -- require 'custom.plugins.goimports'
 -- vim.cmd([[autocmd BufWritePre *.go lua goimports()]])
 require('leap').add_default_mappings()
+
+
+-- Run gofmt + goimport on save
+
+local format_sync_grp = vim.api.nvim_create_augroup("GoImport", {})
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*.go",
+  callback = function()
+    require('go.format').goimport()
+  end,
+  group = format_sync_grp,
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
