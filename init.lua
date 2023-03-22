7e780bc13209f9586817056ca8cd319e8b631acc
David Cohen <dcohen@hashicorp.com>
2023-03-22 07:20:01 -0400

    separate gotests package no longer needed with this go plugin

--- init.lua
+++ init.lua
@@ -114,9 +114,6 @@ require('lazy').setup({
     build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
   {
-    'buoto/gotests-vim',
-  },
-  {
     'ggandor/leap.nvim',
     dependencies = { 'tpope/vim-repeat' },
   },
