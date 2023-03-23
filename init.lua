db38f99babea0891e62c6d10e8a1178297779ad6
David Cohen <dcohen@hashicorp.com>
2023-03-23 14:50:42 -0400

    break themes out of init and move to their own custom file; remove unneeded goimports plugin since the go plugin already does that

--- init.lua
+++ init.lua
@@ -172,42 +172,6 @@ require('lazy').setup({
     },
   },
   {
-    'folke/tokyonight.nvim',
-    priority = 1,
-    config = function()
-      vim.cmd.colorscheme 'tokyonight'
-    end,
-  },
-  {
-    'ishan9299/nvim-solarized-lua',
-  },
-  {
-    'rafamadriz/neon',
-  },
-  {
-    'shaunsingh/nord.nvim',
-    priority = 9,
-    config = function()
-      vim.cmd.colorscheme 'nord'
-    end,
-  },
-  {
-    'dracula/vim',
-    priority = 10,
-    config = function()
-      vim.cmd.colorscheme 'dracula'
-    end,
-  },
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 11,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
-
-  {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
