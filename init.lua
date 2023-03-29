aa785d9a71193a3dc715f92c4c367ab25ec69540
David Cohen <dcohen@hashicorp.com>
2023-03-29 18:34:26 -0400

    install elixir, yaml, etc.

--- init.lua
+++ init.lua
@@ -304,10 +304,10 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim', 'elixir', 'yaml' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-  auto_install = false,
+  auto_install = true,
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
@@ -425,7 +425,9 @@ end
 local servers = {
   -- clangd = {},
   gopls = {},
-  -- pyright = {},
+  elixirls = {},
+  yamlls = {},
+  pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
 
