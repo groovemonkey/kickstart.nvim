87e5d8ceeeef5492c22687844524b28a79022074
David Cohen <dcohen@hashicorp.com>
2023-04-14 08:13:26 -0400

    add iron (repl) plugin

---
+++ after/plugin/defaults.lua
@@ -0,0 +1,6 @@
+-- iron (repl plugin)
+-- iron also has a list of commands, see :h iron-commands for all available commands
+vim.keymap.set('n', '<space>repl', '<cmd>IronRepl<cr>')
+vim.keymap.set('n', '<space>replr', '<cmd>IronRestart<cr>')
+vim.keymap.set('n', '<space>replf', '<cmd>IronFocus<cr>')
+vim.keymap.set('n', '<space>replh', '<cmd>IronHide<cr>')
